c7d143497895d9d717fcd13ebe7cbf8560c231ee
Ihor Huralskyi <uniqueihor@gmail.com>
2021-07-01 15:32:18 +0300

    Add 'id' for books

--- hello-books/book.py
+++ hello-books/book.py
@@ -6,8 +6,8 @@ from flask import jsonify, Response
 
 bp = Blueprint('book', __name__)
 
-books = [{"author": "Brown", "title": "Origin"}, {"author": "Rowling", "title": "Harry Potter"},
-         {"author": "Shevchenko", "title": "Kobzar"}]
+books = [{"id": 1, "author": "Brown", "title": "Origin"}, {"id": 2, "author": "Rowling", "title": "Harry Potter"},
+         {"id": 3, "author": "Shevchenko", "title": "Kobzar"}]
 
 
 @bp.route("/")
@@ -15,27 +15,29 @@ def get_all_books():
     return jsonify(books)
 
 
-@bp.route("/<book_title>")
-def get_book_by_title(book_title):
+@bp.route("/<book_id>")
+def get_book_by_id(book_id):
     for book in books:
-        if book["title"] == book_title:
+        if book["id"] == book_id:
             return jsonify(book)
     return Response(status=404)
 
 
 @bp.route("/create", methods=["POST"])
 def create_book():
+    book_id = request.json.get("id", None)
     title = request.json.get("title", None)
     author = request.json.get("author", None)
 
-    if not title or not author:
+    if not book_id or not title or not author:
         return Response(status=400)
 
     for book in books:
-        if book["title"] == title:
+        if book["id"] == book_id:
             return Response(status=409)
 
     new_book = {
+        "id": book_id,
         "author": author,
         "title": title,
     }
@@ -44,48 +46,28 @@ def create_book():
     return jsonify(new_book), 201
 
 
-@bp.route("/create_query", methods=["POST"])
-def create_book_query():
-    title = request.args.get("title", None)
-    author = request.args.get("author", None)
-
-    if not title or not author:
-        return Response(status=400)
-
-    for book in books:
-        if book["title"] == title:
-            return Response(status=409)
-
-    new_book = {
-        "author": author,
-        "title": title,
-    }
-    books.append(new_book)
-
-    return jsonify(new_book), 201
-
-
-@bp.route("/<book_title>/update", methods=["POST"])
-def update_book(book_title):
+@bp.route("/<book_id>/update", methods=["POST"])
+def update_book(book_id):
     data = request.get_json()
     author = data.get("author", None)
+    title = data.get("title", None)
 
-    if not author:
+    if not author or not title:
         return Response(status=400)
 
     for book in books:
-        if book["title"] == book_title:
+        if book["id"] == book_id:
             book["author"] = author
+            book["title"] = title
             return jsonify(book), 200
 
     return Response(status=404)
 
 
-@bp.route("/<book_title>/delete", methods=["DELETE"])
-def delete_book(book_title):
-
+@bp.route("/<book_id>/delete", methods=["DELETE"])
+def delete_book(book_id):
     for book in books:
-        if book["title"] == book_title:
+        if book["id"] == book_id:
             books.remove(book)
             return jsonify(book), 200
 
